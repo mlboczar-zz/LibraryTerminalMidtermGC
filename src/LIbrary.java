b716dc9c76a5fea1a46ab3945ae449e2123509ae
nickkatz22 <nickkatz22@gmail.com>
2017-04-26 09:52:22 -0400

    Updated Lib and Books

--- src/LIbrary.java
+++ src/LIbrary.java
@@ -4,26 +4,30 @@ import java.util.List;
  * Created by nickk on 4/26/2017.
  */
 public class LIbrary {
+
+
     private final List<LibBooks> books;
 
     public LIbrary(List<LibBooks> books) {
         this.books = books;
     }
     List<LibBooks> loadBooks () {
-        books.add(new LibBooks("Java for Dummies","Antonella"));
+        books.add(new LibBooks("Java for Dummies", "Antonella"));
         books.add(new LibBooks("Captain Underpants","Dav Pilkey"));
         books.add(new LibBooks("IT","Stephen King"));
         books.add(new LibBooks("To Kill a Mockingbird","Harper Lee"));
         books.add(new LibBooks("The Da Vinci Code","Dan Brown"));
         books.add(new LibBooks("A Tale of Two Cities","Charles Dickens"));
         books.add(new LibBooks("The Hobbit","J.R.R. Tolkien"));
-        books.add(new LibBooks("Think and Grow Rich","Napolean Hill"));
+        books.add(new LibBooks("Think and Grow Rich", "Napolean Hill"));
         books.add(new LibBooks("Charlotte's Web","EB White"));
         books.add(new LibBooks("Catcher in the Rye","J.D. Salinger"));
         books.add(new LibBooks("Black Beauty","Anna Sewell"));
         books.add(new LibBooks("Gone with the Wind","Margaret Mitchell"));
         return books;
     }
+
+
 
 
 
--- src/LibBooks.java
+++ src/LibBooks.java
@@ -2,36 +2,39 @@
  * Created by nickk on 4/26/2017.
  */
 public class LibBooks {
-        public enum Status {
-            ON_SHELF,
-            NOT_AVAILABLE
-        }
 
-        private final String title;
-        private final String author;
-        private Status status;
+    private final String title;
+    private final String author;
+    private String status;
 
 
-        public LibBooks(String title, String author) {
-            this.title = title;
-            this.author = author;
-        }
+    public LibBooks(String title, String author) {
+        this.title = title;
+        this.author = author;
 
-        public String getTitle() {
-            return title;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public Status getStatus() {
-            return status;
-        }
-
-        public void setStatus(Status status) {
-            this.status = status;
-        }
     }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return title + "," + author + "," + status;
+    }
+}
 
 
